8723615708fd38f6ad4833cfbdf31e953f63f9be
Vu Van Tai <vutai13196@gmail.com>
2022-07-06 16:53:39 +0700

    state

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,8 +1,29 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    state = {
+        firstName: 'Tai',
+        lastname: 'Vu Van'
+    }
+    handleOnChange = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
     render() {
-        return (<div>How to use class Component in react</div>)
+
+        return (
+            <>
+                <div>
+                    <input value={this.state.firstName} type="text" onChange={(event) => this.handleOnChange(event)}></input>
+                    this is first name: {this.state.firstName}
+                </div>
+                <div>
+                    this is last name: {this.state.lastname}
+                </div>
+            </>
+        )
     }
 }
 export default MyComponent;
