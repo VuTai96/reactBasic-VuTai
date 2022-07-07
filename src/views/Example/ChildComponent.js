f27a74c4313ba292ed5ffa3289b7ce8fd48bf367
Vu Van Tai <vutai13196@gmail.com>
2022-07-08 00:01:10 +0700

    stateless - statefull

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,30 +1,53 @@
 import React from "react";
 
-class ChildComponent extends React.Component {
+/* this is statefull (class component) */
+// class ChildComponent extends React.Component {
 
-    render() {
-        console.log(this.state);
-        console.log(this.props);
-        const { name, age, arrJobs } = this.props;
-        console.log(arrJobs)
-        return (
-            <>
-                <div>
-                    child Component {name} - {age}
-                </div>
-                <div>
-                    {
-                        arrJobs.map((elem, index) => {
-                            return (
-                                <div key={elem.id}>
-                                    {index} - {elem.job} - {elem.salary}
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </>
-        )
-    }
+//     render() {
+//         console.log(this.state);
+//         console.log(this.props);
+//         const { name, age, arrJobs } = this.props;
+//         console.log(arrJobs)
+//         return (
+//             <>
+//                 <div>
+//                     child Component {name} - {age}
+//                 </div>
+//                 <div>
+//                     {
+//                         arrJobs.map((elem, index) => {
+//                             return (
+//                                 <div key={elem.id}>
+//                                     {index} - {elem.job} - {elem.salary}
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
+//             </>
+//         )
+//     }
+// }
+
+/* this is stateless (funtion component) */
+const ChildComponent = (props) => {
+    const { arrJobs } = props;
+    console.log(arrJobs)
+    return (
+        <>
+            <div>
+                {
+                    arrJobs.map((elem, index) => {
+                        return (
+                            <div key={elem.id}>
+                                {index} - {elem.job} - {elem.salary}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
 }
+
 export default ChildComponent;
