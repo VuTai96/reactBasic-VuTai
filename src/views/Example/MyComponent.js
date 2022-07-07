1079c56c7b3cfd0f4c67a33052ac07048f2cae64
Vu Van Tai <vutai13196@gmail.com>
2022-07-07 16:12:00 +0700

    event Dom

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -6,21 +6,31 @@ class MyComponent extends React.Component {
         firstName: 'Tai',
         lastname: 'Vu Van'
     }
-    handleOnChange = (event) => {
+    handleOnChangeName = (event) => {
+
         this.setState({
             firstName: event.target.value
         })
     }
+    handleClickButton() {
+        console.log('my name: ', this.state.firstName)
+        alert(`click me: ${this.state.firstName}`);
+    }
+
+
     render() {
 
         return (
             <>
                 <div>
-                    <input value={this.state.firstName} type="text" onChange={(event) => this.handleOnChange(event)}></input>
+                    <input value={this.state.firstName} type="text" onChange={(event) => this.handleOnChangeName(event)}></input>
                     this is first name: {this.state.firstName}
                 </div>
                 <div>
                     this is last name: {this.state.lastname}
+                </div>
+                <div>
+                    <button onClick={() => this.handleClickButton()}> click me</button>
                 </div>
             </>
         )
