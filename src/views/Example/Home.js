a41df868d5ef5d1a4fd31ff52057f0ad268c61f9
Vu Van Tai <vutai13196@gmail.com>
2022-07-11 23:02:19 +0700

    HOC

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { withRouter } from "react-router";
+import Color from "../HOC/Color";
+
 
 class Home extends React.Component {
+
+    // componentDidMount() {
+    //     setTimeout(() => {
+    //         this.props.history.push('/todo');
+    //         console.log(this.props)
+    //     }, 3000)
+
+    // }
+
     render() {
+        console.log(">>> prop: ", this.props)
         return (
             <div>
                 Hello world from Homepage &amp; with TaiVu-babyShark
@@ -9,4 +22,5 @@ class Home extends React.Component {
         )
     }
 }
-export default Home;
+// export default withRouter(Home);
+export default Color(Home);
