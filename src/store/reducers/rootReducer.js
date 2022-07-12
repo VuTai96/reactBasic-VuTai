c839ae9919cb53ca8aa26c6f16a9d2cfac6b0107
Vu Van Tai <vutai13196@gmail.com>
2022-07-12 18:21:58 +0700

    configure redux

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+    users: [
+        { id: 1, name: 'TaiVu' },
+        { id: 2, name: 'babyShark' }
+    ]
+}
+const rootReducer = (state = initState, action) => {
+    return state;
+}
+
+export default rootReducer;
